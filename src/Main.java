5812133c129347a9065cca801fb164eb0266eeac
toohbias <db2tbo@outlook.de>
2025-05-16 22:04:55 +0200

    autogenerated commit message

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,31 @@ package src;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        BorderPane root = new BorderPane();
+        BorderPane root;
+        try {
+            FileInputStream is = new FileInputStream("Las_Vegas_slot_machines.jpg");
+            Image image = new Image(is);
+            ImageView imageView = new ImageView(image);
+            root = new BorderPane(imageView);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         Scene scene = new Scene(root, 500, 500);
         stage.setScene(scene);
         stage.setTitle("Test");
-        stage.centerOnScreen();
+        stage.setMaximized(true);
         stage.show();
     }
 }
