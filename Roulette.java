47cbaf115e6d27632e6ae59c1ff93adf90d3aad6
Maxi <maxiklemm2@outlook.de>
2025-05-20 11:03:34 +0200

    Spiellogik Methoden anfang

---
+++ Roulette.java
@@ -0,0 +1,47 @@
+public class Roulette {
+
+
+    Roulette()       // Konstruktor
+    {
+
+    }
+
+    public void einsatzFestlegen()  //durch Tasten gewünschten Einsatz festlegen;
+    //überprüfen, ob der aktuelle Betrag den Kontostand üerbschreitet
+    //bei Roulette auf Felder setzten
+    {
+
+    }
+    public void spin()      //methode wartet bis Attribute true ist(man darf spinnen) Methode wird am Ende von CasinoController true gesetzt
+    //waretet auf Tasteninput zum starten Grafikanimation
+    //startet berechnen und zufallszahl
+    //schickt an Grafikfutzi
+    {
+
+    }
+
+    public void berechnen()  //basierend auf festgelegtem Einsatz und gewünschtem Feld wird der Gewinn berechnet
+    // startet automatisch bei Spin
+    // falls gewonnen berechneten Betrag an CasinoController schicken
+    {
+
+    }
+    public int zufallszahl()   //berechnet ob gewonnen oder verloren und gibt berechnen faktor zurück
+    //Slotmachine= 3 Zufallszahlen zwischen 1 und 5 jede Steht für ein Symbol, berechnet daaus dann Gewinnmultiplikator
+    {
+        return 0;
+    }
+
+    public void infoZuGrafiken()    //schickt infos an Grafikfenster; gewonnen oder verloren und berechneten Betrag
+    //Observer controller
+    {
+
+    }
+}
+
+/* Gewinn bei Roulette schwarz/rot und gerade/ungerade 2x
+bei einzelner Zahl x35
+
+ */
+
+
