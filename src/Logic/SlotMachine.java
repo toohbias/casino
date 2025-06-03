ab55cada0d93cb3e6c3ccaa00dc0b638f6b5c0d4
gregorhollfelder <Robin2.Hollfelder@gmx.de>
2025-06-03 09:51:09 +0200

    Verbesserung

--- src/Logic/SlotMachine.java
+++ src/Logic/SlotMachine.java
@@ -15,6 +15,33 @@ public class SlotMachine {
 
     public void spin(int einsatz) throws IllegalAccessException {      //Methode wartet bis Attribute true ist(man darf spinnen) Methode wird am Ende von CasinoController true gesetzt
         if (einsatz <= aktuellerkontostand) {
+            int symbol1 = random.nextInt(5);
+            int symbol2 = random.nextInt(5);
+            int symbol3 = random.nextInt(5);
+
+            if (symbol1 == symbol2) {
+                if (symbol1 == symbol3)
+                {
+                    if (symbol1 == 4)
+                    {
+                        berechnen(50);       //3 siebenen
+                    }
+                    if (symbol1 == 3)
+                    {
+                        berechnen(25);       //3 glocken
+                    }
+                    else
+                    {
+                        berechnen(5);        // 3 gleiche früchte
+                    }
+                }
+            }
+            else if (symbol1 <= 2 && symbol2 <= 2 && symbol3 <= 2)   //3 unterschiedliche Früchte/ egal welche Früchte
+            {
+                berechnen(2);
+            }
+
+            berechnen(0) ;
             zufallszahl();      //brauchen wir Zufallszahl hier? würde die in die berechnen Methode packen
             berechnen(einsatz);
         } else {
@@ -32,42 +59,19 @@ public class SlotMachine {
     // startet automatisch bei Spin,
     // falls gewonnen berechneten Betrag an CasinoController schicken
     {
+        if(einsatz > 0){
         int gewinn = einsatz * zufallszahl();
         aktuellerkontostand = aktuellerkontostand + gewinn;
         System.out.println("Herzlichen Glückwunsch sie haben " + gewinn + "V-Bucks gewonnen");
-        System.out.println("Ihr neuer Kontostand beträgt" + aktuellerkontostand + "V-Bucks");
+        System.out.println("Ihr neuer Kontostand beträgt" + aktuellerkontostand + "V-Bucks"); }
+        else{
+            System.out.println("Niemals Aufgeben");}
     }
 
     public int zufallszahl()   //berechnet, ob gewonnen oder verloren und gibt berechnen Faktor zurück
     //Slotmachine= 3 Zufallszahlen zwischen 1 und 5 jede Steht für ein Symbol, berechnet daaus dann Gewinnmultiplikator
     {
-        int symbol1 = random.nextInt(5);
-        int symbol2 = random.nextInt(5);
-        int symbol3 = random.nextInt(5);
 
-        if (symbol1 == symbol2) {
-            if (symbol1 == symbol3)
-            {
-                if (symbol1 == 4)
-                {
-                    return 50;       //3 siebenen
-                }
-                if (symbol1 == 3)
-                {
-                    return 25;       //3 glocken
-                }
-                else
-                {
-                    return 5;        // 3 gleiche früchte
-                }
-            }
-        }
-        else if (symbol1 <= 2 && symbol2 <= 2 && symbol3 <= 2)   //3 unterschiedliche Früchte/ egal welche Früchte
-        {
-            return 2;
-        }
-
-        return 0;
     }
 
 
