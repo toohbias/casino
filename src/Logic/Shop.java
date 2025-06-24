b51e652c7fce8ea95a54a56f9800272d8f75e945
gregorhollfelder <Robin2.Hollfelder@gmx.de>
2025-06-24 10:55:15 +0200

    SHop Eingabe hinzugeüft

--- src/Logic/Shop.java
+++ src/Logic/Shop.java
@@ -1,13 +1,15 @@
 package src.Logic;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import src.View_GUI.ViewManager;
 
 public class Shop {
-    public static final StringProperty Input = new SimpleStringProperty("");
-    public static void Geldverändern (){
-        double money=0;
+    public static final StringProperty errorMessage = new SimpleStringProperty("");
+    public static final IntegerProperty Input = new SimpleIntegerProperty();
+    public static void Geldverändern (double money){
         ViewManager.getInstance().getController().addMoney(money);
     }
 }
